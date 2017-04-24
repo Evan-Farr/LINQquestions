0e045a9d64dcd0344e375b8cd478350b52f5bedc
Evan-Farr <evan.c.farr@gmail.com>
2017-04-24 15:42:02 -0500

    finished 3

--- LINQ/LINQquestions/Program.cs
+++ LINQ/LINQquestions/Program.cs
@@ -20,32 +20,48 @@ namespace LINQquestions
             //    Console.WriteLine(word);
             //}
 
-            //2
-            var grades = new List<string>()
+            ////2
+            //var grades = new List<string>()
+            //{
+            //    { "90, 60, 45, 85, 85, 90" },
+            //    { "72, 49, 55, 84, 86, 99" },
+            //    { "80, 69, 40, 76, 75, 90" },
+            //};
+            //var averages = new List<double>();
+            //for(int i = 0; i < grades.Count; i++)
+            //{
+            //    var individualStudent = new List<int>();
+            //    Regex regex = new Regex(", ");
+            //    string[] subGrades = regex.Split(grades[i]);
+            //    foreach (var grade in subGrades)
+            //    {
+            //        int intGrade = int.Parse(grade);
+            //        individualStudent.Add(intGrade);
+            //    }
+            //    int min = individualStudent.Min();
+            //    individualStudent.Remove(min);
+            //    double average = individualStudent.Average();
+            //    Console.WriteLine("Student's Average: " + average);
+            //    averages.Add(average);
+            //}
+            //double classAverage = averages.Average();
+            //Console.WriteLine("Class Average: " + Math.Round(classAverage, 2));
+
+            //3
+            var word = new List<string>() { "Llewellyn" };
+            Regex rgx = new Regex("");
+            var subString = rgx.Split(word[0].ToLower());
+            var newWord = subString.OrderBy(l => l).ToList();
+            var query = newWord.AsQueryable().GroupBy(l => l);
+            foreach (var letter in query)
             {
-                { "90, 60, 45, 85, 85, 90" },
-                { "72, 49, 55, 84, 86, 99" },
-                { "80, 69, 40, 76, 75, 90" },
-            };
-            var averages = new List<double>();
-            for(int i = 0; i < grades.Count; i++)
-            {
-                var individualStudent = new List<int>();
-                Regex regex = new Regex(", ");
-                string[] subGrades = regex.Split(grades[i]);
-                foreach (var grade in subGrades)
+                if (!letter.Key.Equals(""))
                 {
-                    int intGrade = int.Parse(grade);
-                    individualStudent.Add(intGrade);
+                    Console.WriteLine($"{letter.Key} {letter.Count()}");
                 }
-                int min = individualStudent.Min();
-                individualStudent.Remove(min);
-                double average = individualStudent.Average();
-                Console.WriteLine("Student's Average: " + average);
-                averages.Add(average);
             }
-            double classAverage = averages.Average();
-            Console.WriteLine("Class Average: " + Math.Round(classAverage, 2));
+
+            //4
         }
     }
 }

c8f4113bb34b000cf898c8e04f5b1ecc46a08651
Evan-Farr <evan.c.farr@gmail.com>
2017-04-24 14:37:05 -0500

    finished prob 2

--- LINQ/LINQquestions/Program.cs
+++ LINQ/LINQquestions/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace LINQquestions
 {
@@ -10,14 +11,14 @@ namespace LINQquestions
     {
         static void Main(string[] args)
         {
-            //1
-            var words = new List<string>() { "hello", "apple", "turtle", "turtle", "pear" };
-            IEnumerable<string> Words = words.Distinct();
-            var alphabeticallyOrderedWords = Words.OrderBy(w => w).ToList();
-            foreach(var word in alphabeticallyOrderedWords)
-            {
-                Console.WriteLine(word);
-            }
+            ////1
+            //var words = new List<string>() { "hello", "apple", "turtle", "turtle", "pear" };
+            //IEnumerable<string> Words = words.Distinct();
+            //var alphabeticallyOrderedWords = Words.OrderBy(w => w).ToList();
+            //foreach(var word in alphabeticallyOrderedWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
 
             //2
             var grades = new List<string>()
@@ -25,8 +26,26 @@ namespace LINQquestions
                 { "90, 60, 45, 85, 85, 90" },
                 { "72, 49, 55, 84, 86, 99" },
                 { "80, 69, 40, 76, 75, 90" },
-                { "91, 89, 45, 59, 81, 100" }
             };
+            var averages = new List<double>();
+            for(int i = 0; i < grades.Count; i++)
+            {
+                var individualStudent = new List<int>();
+                Regex regex = new Regex(", ");
+                string[] subGrades = regex.Split(grades[i]);
+                foreach (var grade in subGrades)
+                {
+                    int intGrade = int.Parse(grade);
+                    individualStudent.Add(intGrade);
+                }
+                int min = individualStudent.Min();
+                individualStudent.Remove(min);
+                double average = individualStudent.Average();
+                Console.WriteLine("Student's Average: " + average);
+                averages.Add(average);
+            }
+            double classAverage = averages.Average();
+            Console.WriteLine("Class Average: " + Math.Round(classAverage, 2));
         }
     }
 }
